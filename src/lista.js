078968ce57ebec9e1e06563c7b7cda8aae863c18
João Victor Macedo Ribeiro <joaovictor.ribeiro@estudante.ufjf.br>
2023-11-21 22:36:59 -0300

    cria e configura lista.js

---
+++ src/lista.js
@@ -0,0 +1,20 @@
+const lista = [
+    "Um",
+    "Dois",
+    "Tres"
+];
+
+function getLista(){
+    return lista;
+}
+
+function adicionaNaLista(){
+
+}
+function removeDaLista(){
+
+}
+function limpaLista(){
+
+}
+export{getLista, adicionaNaLista, removeDaLista, limpaLista};
