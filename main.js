28f43564bca30f09265f5d1e5c6c0e3beb82dbb9
João Victor Macedo Ribeiro <joaovictor.ribeiro@estudante.ufjf.br>
2023-11-21 22:23:03 -0300

    refatora criação dos lis

--- main.js
+++ main.js
@@ -3,14 +3,13 @@ const btnAdicionar = document.querySelector("#adicionar");
 const btnLimpar = document.querySelector("#limpar");
 const olItens = document.querySelector("#itens");
 
-const li = document.createElement("li");
-li.textContent= "Primeiro";
-olItens.appendChild(li)
+adicionaElementoNaLista("Primeiro");
+adicionaElementoNaLista("Segundo");
+adicionaElementoNaLista("Terceiro");
 
-const li2 = document.createElement("li");
-li2.textContent = "Segundo";
-olItens.appendChild(li2)
+function adicionaElementoNaLista(texto){
+    const li = document.createElement("li");
+    li.textContent= texto
+    olItens.appendChild(li)
 
-const li3 = document.createElement("li");
-li3.textContent = "Terceiro";
-olItens.appendChild(li3)
+}
